49a83c3a3f1c280725f1798506dfbbfc9aef45b7
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-22 14:24:22 -0800

    fix edit invoice layout

--- frontend/js/invoice.js
+++ frontend/js/invoice.js
@@ -1,10 +1,14 @@
 const deleteDialog = document.querySelector("#delete-dialog");
+const editDialog = document.querySelector("#edit-invoice-dialog");
 const body = document.querySelector("body");
 
 body.addEventListener("click", (e) => {
   const deleteDialogTarget = e.target.closest("[data-show-delete-dialog]");
+  const editDialogTarget = e.target.closest("[data-show-edit-dialog]");
 
   if (deleteDialogTarget) {
     deleteDialog.showModal();
+  } else if (editDialogTarget) {
+    editDialog.showModal();
   }
 });
