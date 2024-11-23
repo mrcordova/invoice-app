9105c2f95bdee4a6a11458de48d067c7bbb0c7b7
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-22 16:40:29 -0800

    filter invoices logic

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,78 @@
+const data = await (await fetch("data.json")).json();
+
+console.log(data);
+const invoices = document.querySelector(".invoices");
 const newInvoiceDialog = document.getElementById("new-invoice-dialog");
 const newInvoiceBtn = document.querySelector(".new-invoice-btn");
+const main = document.querySelector("main");
+const currencyOptions = { style: "currency", currency: "GBP" };
+const filterOptions = new Set();
+function createInvoices() {
+  for (const invoice of data) {
+    // console.log(invoice);
+    const { id, paymentDue, clientName, total, status } = invoice;
+    invoices.insertAdjacentHTML(
+      "beforeend",
+      `<a href="./invoice.html?invoicd-id=${id}" tabindex="0" data-status="${status}">
+          <div class="invoice league-spartan-bold">
+            <div class="invoice-id">
+              <span>#</span>
+              ${id}
+            </div>
+            <div class="invoice-user league-spartan-medium">${clientName}</div>
+            <div class="due-date league-spartan-medium">Due ${paymentDue}</div>
 
-// console.log(newInvoiceBtn);
+            <div class="amount" data-total="${total}">${parseFloat(
+        total
+      ).toLocaleString("en", currencyOptions)}</div>
+            <div class="status-cont">
+              <div class="status-token" data-status="${status}">
+                <div class="status"></div>
+                ${status}
+              </div>
+              <img
+                class="hide-mobile"
+                src="./assets/icon-arrow-right.svg"
+                alt="right arrow" />
+            </div>
+          </div>
+        </a>`
+    );
+  }
+}
+
+function searchInvoices() {
+  for (const invoice of invoices.children) {
+    // console.log(invoice);
+    invoice.classList.toggle(
+      "hide",
+      !filterOptions.has(invoice.dataset.status) && filterOptions.size != 0
+    );
+  }
+}
+
 newInvoiceBtn.addEventListener("click", (e) => {
   newInvoiceDialog.showModal();
 });
+
+main.addEventListener("click", (e) => {
+  e.preventDefault();
+  const statusFilterOption = e.target.closest("[data-filter-option]");
+  const filterDropdown = e.target.closest("[data-filter-dropdown]");
+
+  if (statusFilterOption) {
+    const input = statusFilterOption.querySelector('[type="checkbox"]');
+    input.checked = !input.checked;
+    if (input.checked) {
+      filterOptions.add(statusFilterOption.dataset.filterOption);
+    } else {
+      filterOptions.delete(statusFilterOption.dataset.filterOption);
+    }
+    searchInvoices();
+    // console.log(filterOptions);
+  } else if (filterDropdown) {
+    const input = filterDropdown.querySelector('[name="filter"]');
+    input.checked = !input.checked;
+  }
+});
+createInvoices();
