921426a4e3d3b36596557ac34047a2772c62a158
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-22 18:38:21 -0800

    logic payment terms update

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -80,9 +80,13 @@ main.addEventListener("click", (e) => {
 });
 
 newInvoiceDialog.addEventListener("click", (e) => {
+  e.preventDefault();
   const cancelBtn = e.target.closest("[data-cancel]");
   const saveBtn = e.target.closest("[data-save]");
-  console.log(e.target);
+  const addItemBtn = e.target.closest("[data-add-item]");
+  const paymentTermsBtn = e.target.closest("[data-payment-terms-option]");
+  const paymentTermInput = e.target.closest("[data-payment-terms-input]");
+  //   console.log(e.target);
   if (cancelBtn) {
     newInvoiceDialog.close();
   } else if (saveBtn) {
@@ -92,6 +96,145 @@ newInvoiceDialog.addEventListener("click", (e) => {
       invoiceForm.reportValidity();
       //   console.log(invoiceForm.checkValidity());
     }
+  } else if (addItemBtn) {
+    if (!addItemBtn.previousElementSibling) {
+      addItemBtn.parentElement.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="invoice-item">
+                  <label class="name">
+                    <span class="label-name">
+                      Item name
+                      <span class="error-text" aria-live="polite"></span>
+                    </span>
+
+                    <input
+                      type="text"
+                      name="items"
+                      id="item-name"
+                      required
+                      autocomplete="off" />
+                  </label>
+                  <label class="qty">
+                    <span class="label-name">Qty.</span>
+                    <input
+                      type="number"
+                      name="items"
+                      id="qty"
+                      required
+                      inputmode="numeric" />
+                  </label>
+                  <label class="price">
+                    <span class="label-name">Price</span>
+                    <input
+                      type="number"
+                      name="items"
+                      id="price"
+                      required
+                      placeholder="0.00"
+                      inputmode="numeric" />
+                  </label>
+                  <label class="total">
+                    <span class="label-name">Total</span>
+                    <input
+                      type="number"
+                      name="items"
+                      id="total"
+                      required
+                      readonly
+                      placeholder="0.00" />
+                  </label>
+                  <button class="delete">
+                    <svg
+                      width="13"
+                      height="16"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z"
+                        fill="currentColor"
+                        fill-rule="nonzero" />
+                    </svg>
+                  </button>
+                </div>`
+      );
+    } else {
+      addItemBtn.previousElementSibling.insertAdjacentHTML(
+        "afterend",
+        `<div class="invoice-item">
+                  <label class="name">
+                    <span class="label-name">
+                      Item name
+                      <span class="error-text" aria-live="polite"></span>
+                    </span>
+
+                    <input
+                      type="text"
+                      name="items"
+                      id="item-name"
+                      required
+                      autocomplete="off" />
+                  </label>
+                  <label class="qty">
+                    <span class="label-name">Qty.</span>
+                    <input
+                      type="number"
+                      name="items"
+                      id="qty"
+                      required
+                      inputmode="numeric" />
+                  </label>
+                  <label class="price">
+                    <span class="label-name">Price</span>
+                    <input
+                      type="number"
+                      name="items"
+                      id="price"
+                      required
+                      placeholder="0.00"
+                      inputmode="numeric" />
+                  </label>
+                  <label class="total">
+                    <span class="label-name">Total</span>
+                    <input
+                      type="number"
+                      name="items"
+                      id="total"
+                      required
+                      readonly
+                      placeholder="0.00" />
+                  </label>
+                  <button class="delete">
+                    <svg
+                      width="13"
+                      height="16"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z"
+                        fill="currentColor"
+                        fill-rule="nonzero" />
+                    </svg>
+                  </button>
+                </div>`
+      );
+    }
+  } else if (paymentTermsBtn) {
+    const parentDiv = paymentTermsBtn.closest("div.net");
+    const inputSpan = parentDiv.querySelector(
+      'span:has(input[type="checkbox"])'
+    );
+    const labelPaymentTerm = parentDiv.querySelector(
+      "[data-payment-terms-value]"
+    );
+    const input = labelPaymentTerm.querySelector("input");
+    inputSpan.querySelector("span").textContent =
+      paymentTermsBtn.querySelector("span").textContent;
+    labelPaymentTerm.setAttribute(
+      "data-payment-terms-value",
+      paymentTermsBtn.dataset.paymentTermsOption
+    );
+    input.checked = !input.checked;
+  } else if (paymentTermInput) {
+    const input = paymentTermInput.querySelector("input");
+    input.checked = !input.checked;
   }
 });
 createInvoices();
