9e27ffb8d04d512ea2484cdeddd41ffcccf35e12
noah cordova <mrcordova@ucdavis.edu>
2024-12-19 09:54:04 -0800

    styling for auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,11 +1,16 @@
-import { hideProgressCircle, showFormErrors, showProgressCircle, URL_WEBSITE  } from "./functions.js";
+import {
+  hideProgressCircle,
+  showFormErrors,
+  showProgressCircle,
+  URL_WEBSITE,
+} from "./functions.js";
 
 function matchPassword(password, repeatPassword) {
   return password === repeatPassword;
 }
 
 function checkPassword(e) {
-  e.target.setCustomValidity('');
+  e.target.setCustomValidity("");
 }
 
 document.addEventListener("click", async (e) => {
@@ -13,93 +18,91 @@ document.addEventListener("click", async (e) => {
   const loginBtn = e.target.closest("[data-login]");
   const signUpBtn = e.target.closest("[data-sign-up]");
   const goPage = e.target.closest("[data-page]");
-  const guestBtn = e.target.closest('[data-guest]');
+  const guestBtn = e.target.closest("[data-guest]");
 
   if (signUpBtn) {
-  
-      const formData = new FormData(signUpBtn.parentElement);
-      const formDataObj = Object.fromEntries(formData.entries());
-      const { password, 'repeat-password': repeatPassword } = formDataObj;
+    const formData = new FormData(signUpBtn.parentElement);
+    const formDataObj = Object.fromEntries(formData.entries());
+    const { password, "repeat-password": repeatPassword } = formDataObj;
 
-      if (showFormErrors(signUpBtn) && matchPassword(password, repeatPassword)) {
-        const btnText = showProgressCircle(signUpBtn);
-        try {
-          const response = await fetch(`${URL_WEBSITE}/registerUser`, {
-            method: "POST",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(formDataObj),
-            cache: 'reload'
-          });
-          if (response.ok) {
-            signUpBtn.parentElement.reset();
-            alert("user successfully registerd");
-            location.href = "login.html";
-          } else {
-            const { message } = await response.json();
-            alert(`Error: ${message}`);
-          }
-        } catch (error) {
-          console.error(`Sign up: ${error}`);
+    if (showFormErrors(signUpBtn) && matchPassword(password, repeatPassword)) {
+      const btnText = showProgressCircle(signUpBtn);
+      try {
+        const response = await fetch(`${URL_WEBSITE}/registerUser`, {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(formDataObj),
+          cache: "reload",
+        });
+        if (response.ok) {
+          signUpBtn.parentElement.reset();
+          alert("user successfully registerd");
+          location.href = "login.html";
+        } else {
+          const { message } = await response.json();
+          alert(`Error: ${message}`);
         }
-        hideProgressCircle(signUpBtn, btnText);
-      } else {
-        // alert('passoword and repeat password do not match');
-        const repeatPasswordInput = signUpBtn.parentElement.querySelector('input[name="repeat-password"]');
-        repeatPasswordInput.setCustomValidity('Do not match password');
-        repeatPasswordInput.addEventListener('input', checkPassword);
+      } catch (error) {
+        console.error(`Sign up: ${error}`);
       }
-  
+      hideProgressCircle(signUpBtn, btnText);
+    } else {
+      // alert('passoword and repeat password do not match');
+      const repeatPasswordInput = signUpBtn.parentElement.querySelector(
+        'input[name="repeat-password"]'
+      );
+      repeatPasswordInput.setCustomValidity("Do not match password");
+      repeatPasswordInput.addEventListener("input", checkPassword);
+    }
   } else if (loginBtn) {
     if (showFormErrors(loginBtn)) {
       const formData = new FormData(loginBtn.parentElement);
       const formObj = Object.fromEntries(formData.entries());
       const btnText = showProgressCircle(loginBtn);
-      
-      try {
 
+      try {
         const response = await fetch(`${URL_WEBSITE}/loginUser`, {
           method: "POST",
           headers: { "Content-type": "application/json" },
           body: JSON.stringify(formObj),
-          cache: 'reload'
+          cache: "reload",
         });
         if (response.ok) {
           loginBtn.parentElement.reset();
-          const { username, img} = await response.json();
-          localStorage.setItem('username', username);
-          localStorage.setItem('img', img);
+          const { username, img } = await response.json();
+          localStorage.setItem("username", username);
+          localStorage.setItem("img", img);
 
           location.href = "/index.html";
         } else {
           const { message } = await response.json();
           alert(`Error: ${message}`);
-        };
-        
+        }
       } catch (error) {
         console.log(`Login: ${error}`);
-      };
+      }
       hideProgressCircle(loginBtn, btnText);
-    };
+    }
   } else if (goPage) {
     location.href = goPage.href;
   } else if (guestBtn) {
     const formData = new FormData();
-    formData.append('username', 'guest');
-    formData.append('password', 'Guest12!');
+    formData.append("username", "guest");
+    formData.append("password", "Guest12!");
     const formObj = Object.fromEntries(formData.entries());
     const btnText = showProgressCircle(guestBtn);
     const response = await fetch(`${URL_WEBSITE}/loginUser`, {
-          method: "POST",
-          headers: { "Content-type": "application/json" },
-          body: JSON.stringify(formObj),
-      cache: 'reload'
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(formObj),
+      cache: "reload",
     });
     if (response.ok) {
-       const { username, img} = await response.json();
-          localStorage.setItem('username', username);
-          localStorage.setItem('img', img);
-          location.href = "/index.html";
-    };
-     hideProgressCircle(guestBtn, btnText);
-  };
+      const { username, img } = await response.json();
+      localStorage.setItem("username", username);
+      localStorage.setItem("img", img);
+      location.href = "/index.html";
+    }
+    hideProgressCircle(guestBtn, btnText);
+  }
 });
